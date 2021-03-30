db858db477123197e82b5abf675cf22a74a5444b
Bima Bagas Raditya <bima.br123@gmail.com>
2021-03-30 23:19:52 +0700

    OS211 bimabr

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -18,7 +18,7 @@ for II in W?? ; do
 				popd
 
 				rm -f $HOME/RESULT/fakeDODOL
-				for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+				for II in $HOME/RESULT/myW*.tar.bz2.asc ; do
 					   echo "Check and move $II..."
 					      [ -f $II ] && mv -f $II .
 				      done
